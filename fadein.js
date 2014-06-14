5a1d410849388ef0dc2a6fcb111f635824978025
Evan Magoni <esm367@gmail.com>
2014-06-13 23:14:38 -0400

    links fade

--- fadein.js
+++ fadein.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('#container').animate({opacity : 1}, 500);
     $('.container').animate({opacity : 1}, 500);
     $('#head').animate({opacity : 1}, 1000);
+    $('#nav a').mouseover(function(){
+        $(this).animate({"opacity": 0.7}, 100);
+    }).mouseout(function(){
+        $(this).animate({"opacity": 1.0}, 100);
+    });
 });
